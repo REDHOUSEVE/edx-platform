459da9e2b06973625d860f095879b7f7210c594d
Sher Ali <sheralim012@gmail.com>
2020-09-22 16:15:35 +0500

    rename old test case and add new test case

--- openedx/features/redhouse_panel/api/v0/tests/test_views.py
+++ openedx/features/redhouse_panel/api/v0/tests/test_views.py
@@ -1,8 +1,12 @@
+from django.conf import settings
 from django.test import TestCase
+from django.test.client import RequestFactory
 from django.urls import reverse
 from rest_framework import status
 
 from openedx.core.djangoapps.site_configuration.tests.factories import SiteConfigurationFactory, SiteFactory
+from openedx.features.edly import cookies
+from openedx.features.edly.tests.factories import EdlySubOrganizationFactory, EdlyUserFactory
 from student.tests.factories import UserFactory
 
 
@@ -32,7 +36,7 @@ class SiteViewTests(TestCase):
         self.assertEqual(response.data['name'], self.site_name)
         self.assertEqual(response.data['address'], self.site_address)
 
-    def test_for_non_staff_user_return_error(self):
+    def test_for_unauthenticated_user_return_error(self):
         site = SiteFactory(name=self.site_name)
         site_configuration = SiteConfigurationFactory(values={'address': self.site_address}, site=site)
         url = reverse('redhouse_features:redhouse_panel:redhouse_panel_api.v0:site', kwargs={'pk': site.id})
@@ -59,3 +63,25 @@ class SiteViewTests(TestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data['name'], site.name)
         self.assertEqual(response.data['address'], '')
+    
+    def test_for_non_staff_user_return_error(self):
+        password = 'fake.password'
+        user = EdlyUserFactory(password=password)
+        site = SiteFactory(name=self.site_name)
+        site_config = SiteConfigurationFactory(values={'address': self.site_address}, site=site)
+        EdlySubOrganizationFactory(lms_site=site)
+        request = RequestFactory()
+        request.user = user
+        request.site = site
+
+        self.client.login(username=user.username, password=password)
+        self.client.cookies.load(
+            {
+                settings.EDLY_USER_INFO_COOKIE_NAME: cookies._get_edly_user_info_cookie_string(request)
+            }
+        )
+        url = reverse('redhouse_features:redhouse_panel:redhouse_panel_api.v0:site', kwargs={'pk': site.id})
+        response = self.client.get(url, SERVER_NAME=site.domain)
+        
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(response.data['detail'], 'You do not have permission to perform this action.')
