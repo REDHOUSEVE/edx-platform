489ab10332629f9f6f1de1450d0f867e7381be4a
Sher Ali <sheralim012@gmail.com>
2020-09-22 11:18:16 +0500

    register url for site view

--- openedx/features/redhouse_panel/api/v0/urls.py
+++ openedx/features/redhouse_panel/api/v0/urls.py
@@ -1,7 +1,12 @@
 """
 URLs for the Admin panel v0 APIs.
 """
+from django.conf.urls import url
+
+from . import views
 
 app_name = 'redhouse_panel_api.v0'
 
-urlpatterns = []
+urlpatterns = [
+    url(r'^site/(?P<pk>\d+)/$', views.SiteView.as_view(), name='site'),
+]
