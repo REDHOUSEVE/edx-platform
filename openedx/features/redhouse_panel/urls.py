41f32fa29711bf5c6175749a194f551fb1a19d7c
danialmalik <danialmalik321@gmail.com>
2020-09-22 11:36:08 +0500

    Update URLs for redhouse panel

--- openedx/features/redhouse_panel/urls.py
+++ openedx/features/redhouse_panel/urls.py
@@ -11,5 +11,5 @@ app_name = 'redhouse_panel'
 urlpatterns = [
     url(r'api/v0/', include('openedx.features.redhouse_panel.api.v0.urls')),
 
-    url(r'', render_redhouse_panel, name='redhouse_panel'),
+    url(r'', render_redhouse_panel, name='index'),
 ]
