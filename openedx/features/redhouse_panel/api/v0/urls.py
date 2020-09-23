c6fec34bdbdd2666d769b9210ff6bc2b18afa228
Luqman-Ud-Din Muhammad <luqman.muhammad@arbisoft.com>
2020-09-23 16:21:46 +0500

    Review Instructor/Student Count API (#47)
    * feat: API to count instructor/student accounts for a site
    * chore: added test cases for UsersAccountStatsAPIView

--- openedx/features/redhouse_panel/api/v0/urls.py
+++ openedx/features/redhouse_panel/api/v0/urls.py
@@ -7,6 +7,12 @@ from openedx.features.redhouse_panel.api.v0 import views
 
 app_name = 'redhouse_panel_api.v0'
 
+
 urlpatterns = [
     url(r'^site/(?P<pk>\d+)/$', views.SiteView.as_view(), name='site'),
+    url(
+        r'^account_stats/?$',
+        views.UsersAccountStatsAPIView.as_view(),
+        name='users_account_stats'
+    ),
 ]
