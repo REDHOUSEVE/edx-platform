ce0180a618fca30cbf7b2b8f5f7d689a6d90ad10
Luqman-Ud-Din <luqmanuddinm@gmail.com>
2020-09-23 15:16:16 +0500

    chore: updated test cases according to API changes

--- openedx/features/redhouse_panel/api/v0/tests/test_users_account_stats.py
+++ openedx/features/redhouse_panel/api/v0/tests/test_users_account_stats.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.test.client import Client
 from django.urls import reverse
 from rest_framework import status
 
@@ -42,18 +43,17 @@ class UsersAccountStatsAPIViewTest(TestCase):
         edly_user_profile = EdlyUserProfileFactory(user=self.student)
         edly_user_profile.edly_sub_organizations.add(*self.edly_sub_organizations)
 
-    def test_with_global_staff_user(self):
-        # is_staff = True
+        self.site = self.staff.edly_profile.edly_sub_organizations.first().lms_site
 
+        self.client = Client(site=self.site, SERVER_NAME=self.site.domain)
+
+    def test_with_global_staff_user(self):
         self.client.login(
             username=self.staff.username,
             password=self.PASSWORD
         )
 
-        url = reverse(
-            'redhouse_panel:redhouse_panel_api.v0:users_account_stats',
-            kwargs={'pk': self.staff.edly_profile.edly_sub_organizations.first().lms_site.id}
-        )
+        url = reverse('redhouse_panel:redhouse_panel_api.v0:users_account_stats')
 
         response = self.client.get(url)
 
@@ -62,10 +62,7 @@ class UsersAccountStatsAPIViewTest(TestCase):
         self.assertEqual(response.data['student_count'], 1)
 
     def test_for_unauthenticated_user_return_error(self):
-        url = reverse(
-            'redhouse_panel:redhouse_panel_api.v0:users_account_stats',
-            kwargs={'pk': self.staff.edly_profile.edly_sub_organizations.first().lms_site.id}
-        )
+        url = reverse('redhouse_panel:redhouse_panel_api.v0:users_account_stats')
 
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
